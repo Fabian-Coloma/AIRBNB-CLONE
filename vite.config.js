2d60927cc490c2457ada51e9c41ae27813c807ce
fabiancoloma <jcolomaatoche7@gmail.com>
2026-06-09 20:51:32 -0500

    segundoavancefiltro3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import { resolve } from 'path'
+import copy from 'rollup-plugin-copy'
 export default defineConfig({
-  base: 'AIRBNB-CLONE', 
+  base: './',
   plugins: [
     tailwindcss(),
+    copy({
+      targets: [
+        { src: 'data.json', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
+
+// hola
