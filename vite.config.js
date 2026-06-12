8e7708260621daf2bbc515c1cc43327b01ef49f4
Fabian Coloma <fabianaca17.123@gmail.com>
2026-06-12 00:08:39 -0500

    fix: removiendo carpeta dist para compilar limpio en vercel

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Ruta relativa universal para que busque en la carpeta donde caiga el deploy
-  base: './', 
   plugins: [
     tailwindcss(),
   ],
